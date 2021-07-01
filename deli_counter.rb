0153cb6d4a4aa27924df7dcc2c1ff7e643889a46
Jacob Loris <jacobloris@outlook.com>
2021-07-01 01:41:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-    customer = [0]
-    counter = 1 
+    puts "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      
     
