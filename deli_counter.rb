77a33bec02b47a710681742412d87bcdee7fbdc4
Jacob Loris <jacobloris@outlook.com>
2021-07-01 02:01:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,11 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else 
-    current = "The line is currently:"
+    currently = "The line is currently:"
     katz_deli.map.with_index do |customer, index|
-      current << " #{index+1}. #{customer}"
+      currently << " #{index+1}. #{customer}"
     end 
-    puts current 
+    puts currently 
   end 
 end 
 
